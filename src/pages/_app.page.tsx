ad8043544f5d991873a29e3b9f73ad650d63bd63
Wellington Albino <well.renato@hotmail.com>
2023-06-14 10:56:18 -0300

    Primeiro commit

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,31 @@
+import { queryClient } from '@/lib/react-query'
+import '../lib/dayjs'
+
+import { globalStyles } from '../styles/global'
+import { SessionProvider } from 'next-auth/react'
+import type { AppProps } from 'next/app'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { DefaultSeo } from 'next-seo'
+
+globalStyles()
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://ignite-call.rocketseat.com.br',
+            siteName: 'Ignite Call',
+          }}
+        />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
+  )
+}
